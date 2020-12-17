7dfa1e029c93ee98f9bf2341a2d104227e24cb36
Elisa <elyflower2001@yahoo.it>
2020-12-17 16:02:09 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function scrollBar() {
+    console.log('scroll')
+
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+} 
